bfd2d6bf1d9c10460dc432b2c6252f37dc503fd3
Pablo Mallea <pablomallea02@gmail.com>
2026-05-27 22:05:08 -0300

    declaro clase modelo RendicionHDRultimaMillaModelo

---
+++ tutasa/RendicionHDRultimaMilla/RendicionHDRultimaMillaModelo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tutasa.RendicionHDRultimaMilla
+{
+    internal class RendicionHDRultimaMillaModelo
+    {
+    }
+}
