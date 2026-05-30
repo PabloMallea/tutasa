4d99e8dc44c795a672f0db88ce723dea9aaa50df
FLORENCIA GISELA RODRIGUEZ PEREZ <87RO37904446@campus.economicas.uba.ar>
2026-05-30 18:19:08 -0300

    Se agrega clase Guia y MovimeintoEstado

---
+++ tutasa/Admision/Guia.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tutasa.Admision
+{
+    internal class Guia
+    {
+        public int NumeroGuia { get; set; }
+
+        public string Cuit { get; set; } = string.Empty;
+
+        public string LocalidadDestino { get; set; } = string.Empty;
+
+        public string Destino { get; set; } = string.Empty;
+
+        public string CalleDestino { get; set; } = string.Empty;
+
+        public string AlturaDestino { get; set; } = string.Empty;
+
+        public string NombreDestinatario { get; set; } = string.Empty;
+
+        public string ApellidoDestinatario { get; set; } = string.Empty;
+
+        public string DniDestinatario { get; set; } = string.Empty;
+
+        public string TelefonoDestinatario { get; set; } = string.Empty;
+
+        public string Dimension { get; set; } = string.Empty;
+
+        public decimal Peso { get; set; }
+
+        public decimal MontoFacturar { get; set; }
+
+        public DateTime FechaAlta { get; set; }
+
+        public int IntentosEntrega { get; set; }
+
+        public bool Facturada { get; set; }
+    }
+}
