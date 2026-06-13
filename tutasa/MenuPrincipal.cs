078618fbf65f0c23ae997b8a30f21f3ca22778d4
Andrés Panitsch <andrespanitsch@hotmail.com>
2026-06-13 16:40:11 -0300

    Menu principal (propuesta)

---
+++ tutasa/MenuPrincipal.cs
@@ -0,0 +1,28 @@
+﻿using tutasa.RuteoTransporte;
+
+namespace tutasa;
+
+public partial class MenuPrincipal : Form
+{
+    public MenuPrincipal()
+    {
+        InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        new FormRuteoTransporte().ShowDialog();
+    }
+
+    private void MenuPrincipal_Load(object sender, EventArgs e)
+    {
+        //cargas comboBox1 con los cds.
+        //comboBox1.Items.Add ...
+    }
+
+    private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        //Estableces Program.CdActual al valor del combo.
+        Program.CdActual = 1;
+    }
+}
